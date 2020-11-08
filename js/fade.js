7d0017ce36adabe7fe98a86ede9f235f2f81cafe
Mikkel Wølk <mikkelwolk@gmail.com>
2020-11-08 15:48:05 +0100

    added js for popup dismiss

---
+++ js/fade.js
@@ -0,0 +1,12 @@
+'use strict';
+
+let cvPopup = document.getElementById('popup');
+let popupClose = document.getElementById('popup_close_btn');
+
+//  Hides the CV popup
+function popupDismiss() {
+    cvPopup.style.opacity = '0';
+    cvPopup.style.zIndex = '-1';
+}
+
+popupClose.addEventListener('click', popupDismiss);
