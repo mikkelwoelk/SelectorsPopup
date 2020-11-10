49b1c6d20b40f27855dc5f6d5005d249864c0a55
Mikkel Wølk <mikkelwolk@gmail.com>
2020-11-10 14:23:13 +0100

    updated classnames

--- js/popup.js
+++ js/popup.js
@@ -1,29 +1,63 @@
 /*Popup JS*/
 
-let cvPopup = document.getElementById('popup');
-let popupClose = document.getElementById('popup_close_btn');
+// Put all logic in an isolated object to limit the scope of the code
+const ctaPopup = {
+    popupClose: document.getElementById('ctaPopup_close_btn'),
+    cvPopup: document.getElementById('ctaPopup'),
 
-//  Displays and fades in popup, 5 sec after the site loads
-const popupShow = async () => {
-    cvPopup.classList.add('show');
-    await delay(5000)
-    if (cvPopup.classList.contains('show')){
-        cvPopup.classList.add('fade-in');
+    // The init method initiates the code
+    init: function() {
+        // Set the cookie name for the cookie that stores whether 
+        // or not the user has closed the pop up already
+        this.cookieName = 'selectors-pop-up';
+
+        // Add event listener for close button
+        // Note: We use bind(this) to bind the scope 
+        // of this ctaPopup-object to the dismiss method
+        this.popupClose.addEventListener('click', this.dismiss.bind(this));
+
+        // Check if the user has dismissed the popup
+        // If not; show the popoup
+        if (!this.hasDismissed()) {
+            this.show();
+        }
+    },
+
+    // Reads the cookie to check if the user has dismissed already
+    hasDismissed: function getCookie() {
+        var v = document.cookie.match('(^|;) ?' + this.cookieName + '=([^;]*)(;|$)');
+        return !!v;
+    },
+
+    show: function() {
+        const popupEl = this.cvPopup;
+        popupEl.classList.add('ctaPopup--show');
+
+        setTimeout(function() {
+            popupEl.classList.add('ctaPopup--fade-in');
+        }, 50);
+    },
+
+    hide: function() {
+        const popupEl = this.cvPopup;
+        popupEl.classList.add('ctaPopup--fade-out');
+        
+        setTimeout(function() {
+            popupEl.classList.remove('ctaPopup--show', 'ctaPopup--fade-in', 'ctaPopup--fade-out');
+            popupEl.classList.add('ctaPopup--hidden');
+        }, 50);
+    },
+
+    // Hide the pop-up and save a cookie that we can check for next time the page loads
+    dismiss: function() {
+        this.hide();
+
+        var d = new Date;
+        var days = 360;
+        d.setTime(d.getTime() + 24 * 60 * 60 * 1000 * days);
+        document.cookie = this.cookieName + "=" + true + ";path=/;expires=" + d.toGMTString();
     }
 }
 
-//  Fades out popup after .5 sec and removes classes, when clicking the close button
-const popupDismiss = async () => {
-    cvPopup.classList.add('fade-out');
-    await delay(500)
-    if (cvPopup.classList.contains('fade-out')){
-        cvPopup.classList.remove('show', 'fade-in', 'fade-out');
-        cvPopup.classList.add('hidden');
-    }
-}
-
-//  Declaring a constant for a delay
-const delay = ms => new Promise(res => setTimeout(res, ms));
-
-popupClose.addEventListener('click', popupDismiss);
-window.addEventListener('load', popupShow);
+// Fire the init method when the DOM loads
+document.addEventListener('DOMContentLoaded', ctaPopup.init());
